77e1d150a867263ae0228d7bb595705c5c2d858d
Multifaces83 <phtchaos@hotmail.com>
2023-03-02 21:25:35 +0100

    Ui Interaction

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Button _validateButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnValidateButtonClicked()
+    {
+        Debug.Log("Validate button clicked");
+
+    }
+}
